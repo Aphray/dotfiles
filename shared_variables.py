b38fff33671a700d4b7cb46ed6da3a04467dd38c
Aphray <brycejkingsley@gmail.com>
2019-01-07 11:47:45 -0500

    Add files via upload

---
+++ shared_variables.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+gradient_colormap = [ '#31A254', '#2BA92B', '#4FAF25', '#7BB61F', '#AEBC18', '#C39B10', '#C96408', '#D02200']
+	
+progress_characters = 		['▁','▂','▃','▄','▅','▆','▇','█']
+progres_intervals = 		[10, 25, 40, 50, 60, 75, 90, 100]
